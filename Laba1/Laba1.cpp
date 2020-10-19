0a4620ad098102e2fe6ae4318cd79f3decdc5ffb
Самсон Квачадзе <lat.lesnik@gmail.com>
2020-10-19 19:29:26 +0300

    various fixes, updated word doc.

--- Laba1/Laba1.cpp
+++ Laba1/Laba1.cpp
@@ -82,7 +82,7 @@ int main()
 	std::cout << std::endl << std::endl;
 	system("pause"); 
 	return 0;
-}
+} 
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
